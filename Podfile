da5dbfbc08963166433f0eb61926bd3b1f609cf8
yanshengge <yanshengge@yangqianguan.com>
2019-09-27 15:04:24 +0800

    config project

---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, '8.0'
+inhibit_all_warnings!
+use_frameworks!
+workspace 'YSViews.xcworkspace'
+
+target 'YSViewsCollector' do
+  project 'YSViewsCollector/YSViewsCollector.xcodeproj'
+  pod 'SnapKit'
+
+end
--- YSViewsCollector/YSViewsCollector/ViewController.swift
+++ YSViewsCollector/YSViewsCollector/ViewController.swift
@@ -7,14 +7,57 @@
 //
 
 import UIKit
+import SnapKit
+
+enum YSViewTitle: String {
+  case menuView = "YSMenuView"
+}
 
 class ViewController: UIViewController {
+  
+  private lazy var dataSource: [YSViewTitle] = [.menuView]
+  
+  private lazy var tableView: UITableView = {
+    let tableView = UITableView()
+    tableView.delegate = self
+    tableView.dataSource = self
+    tableView.register(UITableViewCell.self, forCellReuseIdentifier: "kUITableViewCell")
+    return tableView
+  }()
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    view.backgroundColor = UIColor.white
+    initSubviews()
   }
-
-
+  
+  // MARK: init subviews
+  
+  func initSubviews() {
+    view.addSubview(tableView)
+    tableView.snp.makeConstraints { (make) in
+      make.edges.equalToSuperview()
+    }
+  }
 }
 
+extension ViewController: UITableViewDelegate {
+  
+  func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+    return UIView(frame: .zero)
+  }
+  
+}
+
+extension ViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return dataSource.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "kUITableViewCell", for: indexPath)
+    let viewTitle = dataSource[indexPath.row].rawValue
+    cell.textLabel?.text = viewTitle
+    return cell
+  }
+}

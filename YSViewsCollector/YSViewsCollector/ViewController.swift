4092e4f382b9864e92ee56517bcf3339eeae6640
yanshengge <yanshengge@yangqianguan.com>
2019-09-27 14:12:07 +0800

    add demo project

---
+++ YSViewsCollector/YSViewsCollector/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YSViewsCollector
+//
+//  Created by 葛燕生 on 2019/9/27.
+//  Copyright © 2019 葛燕生. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
